7c62d7bfb3de7ab5c53ae62527ef586c3e29690b
= <=>
2021-06-10 22:25:35 -0400

    calcule small paths in 6 and 19 node

--- main.r
+++ main.r
@@ -1,16 +1,15 @@
 library(igraph)
 
 grafo = make_graph("Dodecahedron")
-plot(grafo)
-
 grafo1 = graph(edges = c(1,2,3,4))
-plot(grafo1)
-
 grafo2 = graph(edges = c(1,2,2,3,3,4,4,1,1,4,4,3,3,2,2,1))
-plot(grafo2)
-
 grafo3 = graph_from_literal(1-+2, 2-+3, 3++4, 4-+1)
-plot(grafo3)
-
 grafo4 = graph_from_literal(1-2, 2-3, 3-4, 4-1)
-plot(grafo4)
+
+start = 6
+end = 19
+plot(grafo)
+# distances(grafo)
+# shortest_paths(grafo, 2)
+all_shortest_paths(grafo, start, end)
+# mean_distance(grafo)
