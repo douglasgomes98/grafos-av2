a06dc0aa638d94a5aaf5c8129d4dcb43e4ba213c
= <=>
2021-06-11 01:10:35 -0400

    calcule small paths in A and Z node

--- main.r
+++ main.r
@@ -1,13 +1,44 @@
 library(igraph)
 
-grafo = make_graph("Dodecahedron")
-grafo1 = graph(edges = c(1,2,3,4))
-grafo2 = graph(edges = c(1,2,2,3,3,4,4,1,1,4,4,3,3,2,2,1))
-grafo3 = graph_from_literal(1-+2, 2-+3, 3++4, 4-+1)
-grafo4 = graph_from_literal(1-2, 2-3, 3-4, 4-1)
+grafo = graph_from_literal( 
+A ++ B ,
+A ++ C ,
+B ++ I ,
+I ++ R ,
+C ++ D ,
+D ++ E ,
+E ++ F ,
+F ++ G ,
+C ++ H ,
+H ++ L ,
+I ++ J ,
+J ++ K ,
+K ++ L ,
+L ++ M ,
+M ++ N ,
+N ++ O ,
+O ++ P ,
+P ++ Q ,
+Q ++ Z ,
+Z ++ Y ,
+Y ++ P ,
+Y ++ X ,
+X ++ O ,
+X ++ W ,
+W ++ N ,
+W ++ V ,
+V ++ M ,
+V ++ U ,
+U ++ L ,
+U ++ T ,
+T ++ K ,
+T ++ S ,
+S ++ J ,
+S ++ R )
 
-start = 6
-end = 19
+start = 'A'
+end = 'Z'
+
 plot(grafo)
 # distances(grafo)
 # shortest_paths(grafo, 2)
